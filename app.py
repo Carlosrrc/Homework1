3def445e561a3192e457a2c08aac13c923364310
Carlosrrc <carlosrrc59@gmail.com>
2024-10-20 14:50:36 +0000

    1

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import textblob
 from transformers import pipeline
 import os
 import google.generativeai as genai
 import random
-import tensorflow as tf
 
 api_key = os.getenv('API_KEY')
 genai.configure(api_key=api_key)
